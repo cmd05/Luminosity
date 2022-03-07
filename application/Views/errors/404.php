0877b1f6801bb58043b9a6c79b0fab4fad09e3ad
Salil <salil.mittal05@gmail.com>
2022-03-08 00:46:01 +0530

    update errors views

--- application/Views/errors/404.php
+++ application/Views/errors/404.php
@@ -1,7 +1,8 @@
 <?php View::header(true, 'Page Not Found') ?>
-<div class="container">
+<div class="container" style='font-family: consolas;'>
     <div class='pt-3'></div>
-    <p class="display-6 text-center pt-5">404 Page Not Found 🕵🏿‍♀️</p>
+    <p class="display-1 text-center pt-5 m-0">404 </p>
+    <p class="text-center pt-4 m-0" style='font-size: 16px'>Page not found 🚧</p>
 </div>
 <br><br><br><br><br>
 <?php View::footer() ?>
